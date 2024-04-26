a1e2e080aa8d28d9bdbea6ef8b96fcd21bc4c736
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-04-26 16:46:24 -0400

    arrays digit and char type counter

---
+++ chapter_1/1_6_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+main() { /* counts digits, white space, others */
+  int c, i, nwhite, nother;
+  int ndigit[10];
+
+  nwhite = nother = 0;
+  for (i = 0; i < 10; ++i)
+    ndigit[i] = 0;
+
+  while((c = getchar()) != EOF)
+    if (c >= '0' && c <= '9')
+      ++ndigit[c-'0'];
+    else if (c == ' ' || c == '\n' || c == '\t')
+      ++nwhite;
+    else
+      ++nother;
+
+  printf("\nDigits:");
+  for(i = 0; i < 10; ++i)
+    printf("\n%d: %d", i, ndigit[i]); 
+  
+  printf("\nWhite Space: %d\nOther: %d\n", nwhite, nother);
+}
